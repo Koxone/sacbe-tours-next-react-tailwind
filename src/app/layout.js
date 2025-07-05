23e32b5f56008b9198ebe97824f57df1cb22ce44
Carlos de Leon <admin@koxland.net>
2025-07-05 15:27:10 -0600

    Add suppressHydrationWarning and head to RootLayout
    Added the suppressHydrationWarning attribute to the html tag and included an empty head element in RootLayout. This helps address hydration mismatches and ensures proper document structure.

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,7 +17,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
+      <head />
       <body>
         <ContextProvider>
           <I18nProvider>
